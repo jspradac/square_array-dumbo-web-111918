7e915aa17eaa4795bed45b2785a223b9b6eca143
Samuel Prada <jspradac@gmail.com>
2018-11-05 20:46:21 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,9 @@
 def square_array(array)
   # your code here
+  squared_array =[]
   array.each do | number |
     squared = number * number
-    sq
+    squared_array.push(squared)
+  end
+  squared_array
 end
